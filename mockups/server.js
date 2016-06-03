295e6e55c64dba52d71fab7b61a3f4ade328a396
Carlos Henrique Lemos <chenriquelemos@gmail.com>
2016-06-03 15:27:12 -0300

    Adicionada mais características na tela de detralhes

--- mockups/server.js
+++ mockups/server.js
@@ -5,7 +5,12 @@ var app = express();
 app.use(bodyParser.json());
 
 var igrejas = [
-    {id: 1, nome: "Igreja Presbiteriana Finsocial", logradouro: "Rua VF-42", complemento:"Quadra 34 Lote 5", bairro: "Vila Finsocial", cidade: {nome: "Goiânia", estado: "GO"}, cep: "74473-380", latitude: -16.622451, longitude: -49.317838, telefone: "(62) 3093-6971"}
+    {id: 1, nome: "Igreja Presbiteriana Finsocial", logradouro: "Rua VF-42", complemento:"Quadra 34 Lote 5", bairro: "Vila Finsocial", cidade: {nome: "Goiânia", estado: "GO"}, cep: "74473-380", latitude: -16.622451, longitude: -49.317838, telefone: "(62) 3093-6971"},
+    {id: 2, nome: "2ª Igreja Presbiteriana de Goiânia", logradouro: "Rua Doutor Inácio Zacarias Pereira, 66", complemento:"Quadra 13", bairro: "Centro Oeste", cidade: {nome: "Goiânia", estado:
+    "GO"}, cep: "74550-070", latitude: -16.664592, longitude: -49.277972, telefone: "(62) 3591-1440"}
+    {id: 3, nome: "2ª Igreja Presbiteriana de Goiânia", logradouro: "Rua Doutor Inácio Zacarias Pereira, 66", complemento:"Quadra 13", bairro: "Centro Oeste", cidade: {nome: "Goiânia", estado: "GO"}, cep: "74550-070", latitude: -16.664592, longitude: -49.277972, telefone: "(62) 3591-1440"}
+    {id: 4, nome: "2ª Igreja Presbiteriana de Goiânia", logradouro: "Rua Doutor Inácio Zacarias Pereira, 66", complemento:"Quadra 13", bairro: "Centro Oeste", cidade: {nome: "Goiânia", estado: "GO"}, cep: "74550-070", latitude: -16.664592, longitude: -49.277972, telefone: "(62) 3591-1440"}
+    {id: 5, nome: "2ª Igreja Presbiteriana de Goiânia", logradouro: "Rua Doutor Inácio Zacarias Pereira, 66", complemento:"Quadra 13", bairro: "Centro Oeste", cidade: {nome: "Goiânia", estado: "GO"}, cep: "74550-070", latitude: -16.664592, longitude: -49.277972, telefone: "(62) 3591-1440"}
 ];
      
 var cidades = [
